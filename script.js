d516a583d5082d2378d93148c0f798de337a6ab0
Stella Gonçalvese <stellagmendonca@gmaile.com>
2025-06-10 15:58:56 -0300

    ajustei fontes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function createHearts() {
 
         // Posiciona os corações aleatoriamente na tela
         heart.style.left = `${Math.random() * 100}vw`; /* Posição horizontal */
-        heart.style.animationDuration = `${Math.random() * 3 + 2}s`; /* Duração da animação */
+        heart.style.animationDuration = `${Math.random() * 3 + 3}s`; /* Duração da animação */
         heart.style.opacity = Math.random(); /* Opacidade aleatória */
 
         heartsContainer.appendChild(heart);
